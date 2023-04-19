38dcc9463bf725014e31a55e838ce6f066560282
trueosiris <tim@chaubet.be>
2023-04-19 14:21:46 +0200

    testing sphinx-autobuild

--- start.sh
+++ start.sh
@@ -4,11 +4,12 @@ if [ ! -f /docs/.sphinx-server.yml ]; then
   echo "copying default config to /docs/.sphinx-server.yml"
   cp /sphinx-server.yml /docs/.sphinx-server.yml
 fi
+cd /docs
 ignorelist=`cat /docs/.sphinx-server.yml | shyaml get-values ignore | xargs` 
 echo "starting sphinx-autobuild..."
 if [ ! -f /docs/source/conf.py ]; then
 	echo "no /docs/source/conf.py found."
 	echo "running basic install"
-	sphinx-quickstart -p docs -a trueosiris -r 0.1 -l en --sep --ext-autodoc /docs
+	sphinx-quickstart -p myproject -a trueosiris -r 0.1 -l en --sep --ext-autodoc /docs
 fi
-sphinx-autobuild --port 8000 --delay 2 /docs/source /docs/build/html --ignore "$ignorelist"
+sphinx-autobuild --port 8000 --delay 2 source build/html --ignore "$ignorelist"
