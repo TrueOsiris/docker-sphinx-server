c9ca3bbd782b13d59b7d6b1ca4edd4e7249231a1
trueosiris <tim@chaubet.be>
2023-11-16 09:50:59 +0100

    correcting script

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ if [ ! -f /docs/source/conf.py ]; then
 	echo "running basic install"
 	sphinx-quickstart -p myproject -a trueosiris -r 0.1 -l en --sep --ext-autodoc /docs
 fi
-if [ $ASCIIDOC=="true" || $ASCIIDOC==true ];then
+if [ $ASCIIDOC=="true" || $ASCIIDOC==true ]; then
 	sphinx-autobuild --host 0.0.0.0 --port 8000 /docs/source /docs/build/html -b asciidoc
 else
 	sphinx-autobuild --host 0.0.0.0 --port 8000 /docs/source /docs/build/html
